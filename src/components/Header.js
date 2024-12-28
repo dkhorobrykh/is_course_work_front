85a38359b03a10097abcd3e15f3e78d6852edb51
DKhorobrykh <horobryh.daniil@yandex.ru>
2024-12-28 16:42:46 +0500

    Скрытие страниц от не админов

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,6 +4,7 @@ import MenuOpenIcon from '@mui/icons-material/MenuOpen';
 import LogoutIcon from '@mui/icons-material/Logout';
 import MakeAdminRequestButton from "./old/MakeAdminRequestButton";
 import {makeAdminRequest} from "../api/api";
+import RoleService from "./RoleService";
 
 const Header = ({onLoginOpen, onRegisterOpen, user, logout, setActiveComponent}) => {
     const [anchorEl, setAnchorEl] = useState(null);
@@ -118,62 +119,56 @@ const Header = ({onLoginOpen, onRegisterOpen, user, logout, setActiveComponent})
         handleMenuClose();
     }
 
-    return (
-        <>
+    return (<>
             <AppBar position="static" style={{zIndex: 10}}>
                 <Toolbar>
                     <Button color="inherit" onClick={handleMenuOpen}><MenuOpenIcon/></Button>
                     <Menu anchorEl={anchorEl} open={Boolean(anchorEl)} onClose={handleMenuClose}>
                         <MenuItem onClick={handleMainClick}>Main page</MenuItem>
-                        <MenuItem onClick={handleBookedFlightsClick}>Booked flights</MenuItem>
-                        <MenuItem onClick={handleCargoClick}>Cargo</MenuItem>
-                        <MenuItem onClick={handleChatClick}>Chat</MenuItem>
-                        <MenuItem onClick={handleUserDocsClick}>Documents</MenuItem>
-                        <MenuItem onClick={handleFlightsClick}>Flights</MenuItem>
-                        <MenuItem onClick={handleGalaxy}>Galaxy</MenuItem>
 
-                        <MenuItem onClick={() => {}} style={{background: "#e13535"}}>----------------------</MenuItem>
-                        <MenuItem onClick={() => {}} style={{background: "#e13535"}}>| ADMIN ZONE |</MenuItem>
-                        <MenuItem onClick={() => {}} style={{background: "#e13535"}}> vvvvvvvvvvvvv</MenuItem>
+                        {user && (<>
+                            <MenuItem onClick={handleBookedFlightsClick}>Booked flights</MenuItem>
+                            <MenuItem onClick={handleCargoClick}>Cargo</MenuItem>
+                            <MenuItem onClick={handleChatClick}>Chat</MenuItem>
+                            <MenuItem onClick={handleUserDocsClick}>Documents</MenuItem>
+                            <MenuItem onClick={handleFlightsClick}>Flights</MenuItem>
+                            <MenuItem onClick={handleGalaxy}>Galaxy</MenuItem>
+                        </>)}
 
-                        <MenuItem onClick={handleAnalysisClick}>Analysis</MenuItem>
-                        <MenuItem onClick={handleManageFlightsClick}>Manage flights</MenuItem>
-                        <MenuItem onClick={handleCheck}>Pre Flight Checking</MenuItem>
-                        <MenuItem onClick={handleRoleClick}>Roles</MenuItem>
-                        <MenuItem onClick={handleUserRoleClick}>User roles</MenuItem>
-                        <MenuItem onClick={handleSchedulesClick}>Schedules</MenuItem>
-                        <MenuItem onClick={handleShipsClick}>Ships</MenuItem>
-                        <MenuItem onClick={handleInsuranceProgramClick}>Insurance programs</MenuItem>
-
-                        {/*{ user && (*/}
-                        {/*    <MenuItem onClick={handleVehicleImportClick}>Import vehicles</MenuItem>*/}
-                        {/*)}*/}
-                        {/*{user && user.admin && (*/}
-                        {/*    <MenuItem onClick={handleAdminRequestsClick}>Admin requests table</MenuItem>*/}
-                        {/*)}*/}
+                        {user && RoleService.isAdmin(user) && (<>
+                                <MenuItem onClick={() => {
+                                }} style={{background: "#e13535"}}>----------------------</MenuItem>
+                                <MenuItem onClick={() => {
+                                }} style={{background: "#e13535"}}>| ADMIN ZONE |</MenuItem>
+                                <MenuItem onClick={() => {
+                                }} style={{background: "#e13535"}}> vvvvvvvvvvvvv</MenuItem>
+                                <MenuItem onClick={handleAnalysisClick}>Analysis</MenuItem>
+                                <MenuItem onClick={handleManageFlightsClick}>Manage flights</MenuItem>
+                                <MenuItem onClick={handleCheck}>Pre Flight Checking</MenuItem>
+                                <MenuItem onClick={handleRoleClick}>Roles</MenuItem>
+                                <MenuItem onClick={handleUserRoleClick}>User roles</MenuItem>
+                                <MenuItem onClick={handleSchedulesClick}>Schedules</MenuItem>
+                                <MenuItem onClick={handleShipsClick}>Ships</MenuItem>
+                                <MenuItem onClick={handleInsuranceProgramClick}>Insurance programs</MenuItem>
+                            </>)}
                     </Menu>
 
                     <Typography variant="h6" sx={{flexGrow: 1}}>
-                        IS   |   🪐 Intergalactic transportation 🪐   |   Course project
+                        IS | 🪐 Intergalactic transportation 🪐 | Course project
                     </Typography>
 
-                    {user ? (
-                        <>
+                    {user ? (<>
                             <Typography variant="body1" sx={{mr: 2}}>
                                 Hello, {user.surname} {user.firstName} {user.lastName}!
                             </Typography>
                             <Button color="inherit" onClick={logout}><LogoutIcon/></Button>
-                        </>
-                    ) : (
-                        <>
+                        </>) : (<>
                             <Button color="inherit" onClick={onLoginOpen}>Login</Button>
                             <Button color="inherit" onClick={onRegisterOpen}>Register</Button>
-                        </>
-                    )}
+                        </>)}
                 </Toolbar>
             </AppBar>
-        </>
-    );
+        </>);
 };
 
 export default Header;
